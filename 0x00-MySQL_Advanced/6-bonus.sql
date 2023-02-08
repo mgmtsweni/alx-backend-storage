a98ef9d5c89766f5dfa4b58ea767ad7f46f68c9e
mgmtsweni <mtswenigift@gmail.com>
2023-02-08 20:32:46 +0200

    Task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,7 +1,8 @@
 --- creates a stored procedure AddBonus
 --- that adds a new correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
-CREATE PROCEDURE AddBonus ( user_id INT, project_name VARCHAR(225), score FLOAT)
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
     DECLARE project_count INT DEFAULT 0;
     DECLARE project_id INT DEFAULT 0;
